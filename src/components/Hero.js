b674a81b8f188bc9a4eaf925a38f8f15c21e4df6
alfredo <alfredocapellan20@gmail.com>
2020-06-07 22:20:28 -0400

    Reponsive navbar

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+export const Hero = styled.div``;
 
 export default class Hero extends Component {
   render() {
